4e6a42246b2aeb91959031f67c1ed95a581dc465
smayilmaz11 <esmnyilmaz@hotmail.com>
2020-05-13 15:37:28 +0300

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            string dosya = @"dosya.xml";
+            xmldoc.Load(dosya);
+
+            xmldoc.Save(Console.Out);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # siberfinal
+# siberfinal
